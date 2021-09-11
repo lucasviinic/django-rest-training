d5948a9fd90fad0cb018b1c460bebdc7b43bd3e6
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2021-09-11 10:43:15 -0300

    Viewset, url e requisições

--- school/views.py
+++ school/views.py
@@ -1,7 +1,14 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from school.models import Aluno, Curso
+from school.serializer import AlunoSerializer, CursoSerializer
 
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {'id': 1, 'nome': 'Lucas'}
-        return JsonResponse(aluno)
+class AlunosViewSet(viewsets.ModelViewSet):
+    '''Exibindo todos os alunos e alunas'''
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursosViewSet(viewsets.ModelViewSet):
+    '''Exibindo todos os cursos'''
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
