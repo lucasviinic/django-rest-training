211f4fc57d40aaa8fcef25e3862e7771d7e49f51
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2021-09-20 20:45:15 -0300

    Validações no Serializer

--- django-validacoes-buscas-filtros-deploy/clientes/serializers.py
+++ django-validacoes-buscas-filtros-deploy/clientes/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import * 
+
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
-    
+
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf': 'O CPF deve ter 11 dígitos'})
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome': 'O nome deve conter apenas letras'})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg': 'O Rg deve ter 9 dígitos'})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular': 'O celular deve ter 11 dígitos'})
+        return data
---
+++ django-validacoes-buscas-filtros-deploy/virtualenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\source\studies\django-rest-training\django-validacoes-buscas-filtros-deploy\virtualenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
