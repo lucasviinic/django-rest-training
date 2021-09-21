074c0b0a802cb61b2a00e618a179bdc5b4af07ce
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2021-09-21 09:15:56 -0300

    Expressões regulares

--- django-validacoes-buscas-filtros-deploy/clientes/serializers.py
+++ django-validacoes-buscas-filtros-deploy/clientes/serializers.py
@@ -16,5 +16,5 @@ class ClienteSerializer(serializers.ModelSerializer):
         if not rg_valido(data['rg']):
             raise serializers.ValidationError({'rg': 'O Rg deve ter 9 dígitos'})
         if not celular_valido(data['celular']):
-            raise serializers.ValidationError({'celular': 'O celular deve ter 11 dígitos'})
+            raise serializers.ValidationError({'celular': 'O número de celular deve seguir este modelo 81 99090-9090 (respeitando os espaços e traços)'})
         return data
